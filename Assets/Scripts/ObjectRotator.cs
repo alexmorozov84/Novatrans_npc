a4b68d46863340a139be120015977e4a547ec687
aim26 <ya@aim26.com>
2024-06-04 02:01:47 +0500

    some adds

--- Assets/Scripts/ObjectRotator.cs
+++ Assets/Scripts/ObjectRotator.cs
@@ -10,11 +10,15 @@ public class ObjectRotator : MonoBehaviour
     {
         if (target != null && Input.GetMouseButton(0))
         {
-            float h = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
-            float v = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+            // Проверка, находится ли курсор в правой части экрана
+            if (Input.mousePosition.x > Screen.width * 0.3f)
+            {
+                float h = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+                float v = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
 
-            target.Rotate(Vector3.up, -h, Space.World);
-            target.Rotate(Vector3.right, v, Space.World);
+                target.Rotate(Vector3.up, -h, Space.World);
+                target.Rotate(Vector3.right, v, Space.World);
+            }
         }
     }
 }
